b831e06c8174b6dd3355f1ef34a82a941658fc57
supreeth <supreethratam@gmail.com>
2024-02-04 15:51:34 +0530

    aded netlify config

--- functions/bot/app.js
+++ functions/bot/app.js
@@ -90,5 +90,15 @@ bot.on("chosen_inline_result", async (ctx) => {
   }
 });
 
-bot.launch();
-console.log("The bot has been started...");
+exports.handler = async (event) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
+  }
+};
